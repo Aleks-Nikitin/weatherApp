8eb94d42804719b5a4818d2138c9d0d1f5c3d3ee
Power366 <172065612+Power366@users.noreply.github.com>
2025-07-30 13:56:14 +0700

    switch between °C and °F, clear before submit

--- src/index.js
+++ src/index.js
@@ -1,45 +1,80 @@
 import "./reset.css";
 import "./styles.css";
-function createReferences(){
-    return {
-        input:document.querySelector("input"),
-        sumbitBtn: document.querySelector("#submit"),
-        content:document.querySelector(".content")
-    }
+function createReferences() {
+  return {
+    input: document.querySelector("input"),
+    sumbitBtn: document.querySelector("#submit"),
+    content: document.querySelector(".content"),
+    nodeDegrees: document.querySelectorAll("#celsius,#farenheit"),
+    celcius: document.querySelector("#celcius"),
+    farenheit: document.querySelector("#farenheit"),
+    container: document.querySelector(".container"),
+  };
 }
-let accessDOM =createReferences();
-class WheatherData{
-    constructor(info){
-        this.info =info;
-    }
-    showUI(){
-        let block = document.createElement("div");
-        let information = document.createElement("p");
-        accessDOM.content.appendChild(block);
-        block.appendChild(information);
-        information.textContent = this.info;
-    }
+let metrics = true;
+let accessDOM = createReferences();
+class WheatherData {
+  constructor(info) {
+    this.info = info;
+  }
+  showUI() {
+    let block = document.createElement("div");
+    let information = document.createElement("p");
+    accessDOM.content.appendChild(block);
+    block.appendChild(information);
+    information.textContent = this.info;
+  }
 }
-/*
-function showWheatherData(){
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-        
-    }
-} */
-accessDOM.sumbitBtn.addEventListener("click",()=>{
-    getData().then((result) => {
-    new WheatherData(`Current Time: ${result.currentConditions.datetime}`).showUI();
-    new WheatherData(`Current conditions:${result.currentConditions.conditions}`).showUI();
-    new WheatherData(`Current temperature: ${result.currentConditions.temp}`).showUI();
-    new WheatherData(`Current wind speed : ${result.currentConditions.windspeed}`).showUI();
-    new WheatherData(result.description).showUI();
+accessDOM.celcius.addEventListener("click", () => {
+  metrics = true;
+});
+accessDOM.farenheit.addEventListener("click", () => {
+  metrics = false;
+});
+function clear() {
+  accessDOM.content.innerHTML = "";
 }
-)
-
-})
-async function getData(){
-    const result = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${accessDOM.input.value}?unitGroup=metric&key=DRT4FPTHB3J4QG5EQACVB7T4Z&contentType=json`,{mode:'cors'});
+accessDOM.sumbitBtn.addEventListener("click", () => {
+  clear();
+  getData()
+    .then((result) => {
+      clear();
+      return result;
+    })
+    .then((result) => {
+      new WheatherData(
+        `Current Time: ${result.currentConditions.datetime}`,
+      ).showUI();
+      new WheatherData(
+        `Current conditions:${result.currentConditions.conditions}`,
+      ).showUI();
+      new WheatherData(
+        `Current temperature: ${result.currentConditions.temp}`,
+      ).showUI();
+      new WheatherData(
+        `Current wind speed : ${result.currentConditions.windspeed}`,
+      ).showUI();
+      new WheatherData(result.description).showUI();
+    })
+    .catch(() => {
+      throw new Error("No data");
+    });
+});
+async function getData() {
+  let url;
+  if (metrics) {
+    url = "metric";
+  } else {
+    url = "us";
+  }
+  try {
+    const result = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${accessDOM.input.value}?unitGroup=${url}&key=DRT4FPTHB3J4QG5EQACVB7T4Z&contentType=json`,
+      { mode: "cors" },
+    );
     const data = await result.json();
-    return data
+    return data;
+  } catch {
+    throw new Error("couldnt get data");
+  }
 }
